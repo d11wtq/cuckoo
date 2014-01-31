0ade4e5c5318d7cd11fd016329caf1de5d3e642b
d11wtq <chris@w3style.co.uk>
2014-01-31 21:27:12 +1100

    Incomplete initial cron parsing commit

--- src/workkit/cron.clj
+++ src/workkit/cron.clj
@@ -2,62 +2,42 @@
   "WorkKit cron parsing functions."
   (:require [clj-time.core :as time]
             [clj-time.local :as local-time])
+  (:use [clojure.string :only [split]])
   (:import java.util.Date
            org.joda.time.DateTime))
 
-(defn parse-at
-  "Parses `text`, which refers to a point in time and returns a cron line."
-  [time-text]
-  (let [date (time/to-time-zone
-               (local-time/to-local-date-time time-text)
-               (time/time-zone-for-offset 0))]
-    (format "%d %d %d %s %d %d %d"
-            (time/year date)
-            (time/month date)
-            (time/day date)
-            (time/day-of-week date)
-            (time/hour date)
-            (time/minute date)
-            (time/second date))))
 
-(defn parse-in
-  [text])
+(defn add-default-values
+  [values]
+  (case (count values)
+    5 (into ["0"] (conj (vec values) "*"))
+    7 values
+    nil))
 
-(defn parse-every
-  [text])
+(defn parse-numeric
+  [value]
+  (vec (map #(Integer. %) (split value #","))))
 
-(defn build
-  "Build a WorkKit cron line for the given `time-type` and `time-text`."
-  [time-type time-text]
-  ((time-type
-     {:at    parse-at
-      :in    parse-in
-      :every parse-every}) time-text))
+(defn parse-values
+  [values]
+  (if-let [values (add-default-values values)]
+    (let [parse-fns {:second      parse-numeric
+                     :minute      identity
+                     :hour        identity
+                     :month       identity
+                     :day         identity
+                     :day-of-week identity
+                     :year        identity}]
+      (apply hash-map
+             (flatten
+               (map (fn [value [field parse-fn]]
+                      [field (parse-fn value)])
+                    values
+                    parse-fns))))))
 
-(defn parse-cron
-  "Parse the `cron-string` and return a map of the component parts."
-  [cron-string]
-  (zipmap [:year
-           :month
-           :day
-           :day-of-week
-           :hour
-           :minute
-           :second]
-          (map #(Integer. %)
-               (clojure.string/split cron-string #"[ \t]"))))
-
-(defn next-time
-  "Returns the next time after `date` that matches the given `cron` line."
-  [cron-string java-date]
-  (let [cron (parse-cron cron-string)
-        date (time/to-time-zone
-               (DateTime. java-date)
-               (time/time-zone-for-offset 0))]
-    (.toDate (time/date-time
-               (or (:year cron) (time/year date))
-               (or (:month cron) (time/month date))
-               (or (:day cron) (time/day date))
-               (or (:hour cron) (time/hour date))
-               (or (:minute cron) (time/minute date))
-               (or (:second cron) (time/second date))))))
+(defn load-str
+  "Parse a cron string into a structured map of its component parts."
+  [cron-str]
+  (or (parse-values (split cron-str #"[ \t]+"))
+      (throw (IllegalArgumentException.
+               (format "Invalid cron string: %s" cron-str)))))
--- test/workkit/cron_test.clj
+++ test/workkit/cron_test.clj
@@ -4,16 +4,18 @@
   (:import java.util.Date))
 
 (deftest cron-test
-  (testing "workkit.cron/build"
-    (testing ":at"
-      (testing "with an exact date"
-        (testing "sets all fields"
-          (is (= "2014 3 2 7 9 0 0" ; 7 = Sun
-                 (cron/build :at "2014-03-02T09:00:00Z")))))))
+  (testing "workkit.cron/load-str"
+    (testing "for a 5-component cron string"
+      (testing "returns a map of all components"
+        (is (every? #{:year
+                      :month
+                      :day
+                      :day-of-week
+                      :hour
+                      :minute
+                      :second}
+                    (keys (cron/load-str "* * * * *")))))
 
-  (testing "workkit.cron/next-time"
-    (testing "with all fields set"
-      (testing "returns the exact date"
-        (is (= (Date. (- 2014 1900) (- 3 1) 2 9 0 0)
-               (cron/next-time "2014 3 2 7 9 0 0"
-                               (Date. (- 2014 1900) (- 3 1) 2 8 0 0))))))))
+      (testing "sets the :second to zero"
+        (is (= [0]
+               (:second (cron/load-str "* * * * *"))))))))
