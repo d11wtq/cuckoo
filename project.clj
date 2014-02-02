74ce88aad004327d5e17e5add04ff8b0bd3f5c76
d11wtq <chris@w3style.co.uk>
2014-02-02 16:23:11 +1100

    Fix up project.clj

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject workkit "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+(defproject cuckoo "0.1.0-SNAPSHOT"
+  :description "Cuckoo - Distributed scheduling & queuing for Clojure"
+  :url "https://github.com/d11wtq/cuckoo"
+  :license {:name "MIT License"
+            :url "http://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/tools.macro "0.1.1"]
                  [com.taoensso/carmine "2.4.5"]
